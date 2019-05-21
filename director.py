68489ce964cc8d5f55b568b8c8abdfa3c9ab9f7d
cey0030 <48426058+cey0030@users.noreply.github.com>
2019-05-21 13:25:30 +1200

    Update director.py

--- director.py
+++ director.py
@@ -4,12 +4,9 @@ class Director(object):
 
     def build_class(self, class_item):
         self.__builder.add_class_name(class_item)
-
         self.__builder.add_attributes(class_item)
-
         self.__builder.add_relationships(class_item)
-
         self.__builder.add_methods(class_item)
-        return self.__builder.get_result()
+
 
 
